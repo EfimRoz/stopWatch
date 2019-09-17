feebc2344faa97882ee809fec74224e01a0a0f36
Fima <FimaRozovsky@hotmail.com>
2019-09-17 18:07:32 +0300

    Improved the design of the watch screen, and the buttons bar

--- src/app/stylize-number.directive.ts
+++ src/app/stylize-number.directive.ts
@@ -13,8 +13,8 @@ export class StylizeNumberDirective {
     if(changes.number){
       console.log('input changed');
       if(this.number.toString(10).includes('1')) {
-        this.el.nativeElement.style.letterSpacing = '5px';
-        this.el.nativeElement.style.marginRight = '-5px';
+        this.el.nativeElement.style.letterSpacing = '9px';
+        this.el.nativeElement.style.marginRight = '-9px';
     
       }
     }
